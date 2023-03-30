f87c143ed3d88180d7d16cb9f0e5932eac7af254
Kaniniwambua <kaniniwambua97@gmail.com>
2023-03-30 10:58:47 -0700

    write function that prints a string of character before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* prints a string
+ * should return 0
+ */
+int main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+return 0;
+}
