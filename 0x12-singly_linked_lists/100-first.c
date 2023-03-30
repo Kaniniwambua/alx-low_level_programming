772d56e327b5e9494c134e3dddcb5cc287f84112
Kaniniwambua <kaniniwambua97@gmail.com>
2023-03-30 11:11:22 -0700

    write a function that prints a string before main is is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
-/* prints a string
- * should return 0
+#include<stdio.h>
+
+/**
+ * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+ *   is executed before main()
  */
-int main()
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-return 0;
 }
