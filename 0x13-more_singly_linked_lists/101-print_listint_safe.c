d3ccaf23d62f683d404686a2845c3b249011c771
Kaniniwambua <kaniniwambua97@gmail.com>
2023-04-05 12:48:34 -0700

    prints lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,39 +1,39 @@
 #include "lists.h"
 
 /**
- * print_list_safe - Prints a list.
+ * print_listint_safe - Prints a list.
  * @head: a pointer to the head node
  * Return: number of nodes
  */
-size_t print_list_safe(const listint_t *head)
+size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *current = head, *runner = head;
-    size_t count = 0;
+	const listint_t *current = head, *runner = head;
+	size_t count = 0;
 
-    while (current != NULL && runner != NULL && runner->next != NULL)
-    {
-        printf("[%p] %s\n", (void *)current, current->str);
-        count++;
-        current = current->next;
-        runner = runner->next->next;
+	while (current != NULL && runner != NULL && runner->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+		current = current->next;
+		runner = runner->next->next;
 
-        if (current == runner)
-        {
-            current = head;
+		if (current == runner)
+		{
+			current = head;
 
-            while (current != runner)
-            {
-                printf("[%p] %s\n", (void *)current, current->str);
-                count++;
-                current = current->next;
-                runner = runner->next;
-            }
+			while (current != runner)
+			{
+				printf("[%p] %d\n", (void *)current, current->n);
+				count++;
+				current = current->next;
+				runner = runner->next;
+			}
 
-            printf("[%p] %s\n", (void *)current, current->str);
-            count++;
-            break;
-        }
-    }
+			printf("[%p] %d\n", (void *)current, current->n);
+			count++;
+			break;
+		}
+	}
 
-    return (count);
+	return (count);
 }
