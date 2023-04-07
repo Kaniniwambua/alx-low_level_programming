fe44dcb40b4b7a020b49e11ac96a424351c70d2a
Kaniniwambua <kaniniwambua97@gmail.com>
2023-04-07 05:08:42 -0700

    Write a function that prints the binary representation of a number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * binary_to_uint - converts a binary number to unsigned integer
  * @b: pointer to string of binary characters
  * Return: converted number or 0
  */
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 
@@ -12,22 +13,19 @@ unsigned int binary_to_uint(const char *b);
 	{
 		return (0);
 	}
-	for (int i = 0; b[i] = "\0"; i++)
+	for (int i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == "0")
+		if (b[i] == '0')
 		{
-		num = num * 2;
+			num = num * 2;
 		}
-	else if (b[i] == "1")
-	{
-		num = num * 2 + 1;
-
-	}
-	else
-	{
-			return (0);
+		else if (b[i] == '1')
+		{
+			num = num * 2 + 1;
+		}
+		else
+		{return (0);
+		}
 	}
 	return (num);
-	}
 }
-
