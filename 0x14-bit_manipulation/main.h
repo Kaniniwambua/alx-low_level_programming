8b7dd046655bad3bb341dedef57b71784e95ec03
Kaniniwambua <kaniniwambua97@gmail.com>
2023-05-04 14:13:11 -0700

    sets value of bit to 1 given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _putchar(char c);
 unsigned long int _power(unsigned int base, unsigned int pow);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
